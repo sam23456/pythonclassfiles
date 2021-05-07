0dcb916bf60af22592f7571e3d9aa4e8eb0f8e71
sam23456 <47560138+sam23456@users.noreply.github.com>
2021-05-07 23:12:56 +0530

    Add files via upload

---
+++ assignment 06-05-2021 (1).py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+first_name = 'sanjay kumar'
+
+
+# In[2]:
+
+
+print(first_name)
+
+
+# In[3]:
+
+
+last_name = 'n'
+
+
+# In[4]:
+
+
+fullname = f'{first_name}{last_name}'
+
+
+# In[5]:
+
+
+print(fullname)
+
+
+# In[6]:
+
+
+fullname1 = f'{first_name} {last_name}'
+
+
+# In[7]:
+
+
+print(fullname1)
+
+
+# In[8]:
+
+
+print(fullname1.title())
+
+
+# In[9]:
+
+
+print(fullname1.upper())
+
+
+# In[10]:
+
+
+print(fullname1.lower())
+
+
+# In[11]:
+
+
+print =(f'wellcome,{fullname1.title()}')
+
+
+# In[16]:
+
+
+fullname1 = f'{first_name} {last_name}'
+
+
+# In[18]:
+
+
+print(f'wellcome,{fullname1.title()}')
+
+
+# In[19]:
+
+
+first_name2 = 'sanjay'
+
+
+# In[20]:
+
+
+last_name2 = 'kumar'
+
+
+# In[29]:
+
+
+fullname2 = f"{first_name2}{last_name2}"
+
+
+# In[ ]:
+
+
+
+
